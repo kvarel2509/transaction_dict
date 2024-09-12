acd47f64de1d446240d6ddee312cca613e52afeb
aparshin <a.parshin@mediasoft.team>
2024-09-12 17:27:19 +0300

    test: add test for transaction multi-version strategy

--- tests/test_transactions_with_multi_version_strategy.py
+++ tests/test_transactions_with_multi_version_strategy.py
@@ -7,7 +7,7 @@ from src.exceptions import SerializationError
 factory = InMemoryJournalRepositoryFactory()
 
 
-class TransactionTestsMixin(TestCase):
+class TransactionTestsMixin:
     isolation_level: IsolationLevel
 
     def setUp(self):
@@ -34,10 +34,6 @@ class TransactionTestsMixin(TestCase):
                 journal_repository=factory.get_journal_repository(),
             )
         )
-
-
-class ReadCommittedTransactionTestCase(TransactionTestsMixin, TestCase):
-    isolation_level = IsolationLevel.READ_COMMITTED
 
     def test_cannot_occur_lost_update_when_concurrent_rewrite_diff_values(self):
         self.transaction1[self.key1] = self.value2
@@ -82,31 +78,24 @@ class ReadCommittedTransactionTestCase(TransactionTestsMixin, TestCase):
         self.transaction2[self.key1] = self.value2
         self.assertEqual(self.transaction1[self.key1], self.value1)
 
+
+class ReadCommittedTransactionTestCase(TransactionTestsMixin, TestCase):
+    isolation_level = IsolationLevel.READ_COMMITTED
+
     def test_can_occur_non_repeatable_read(self):
         self.transaction2[self.key1] = self.value2
         self.transaction2.commit()
         self.assertEqual(self.transaction1[self.key1], self.value2)
 
     def test_can_occur_phantoms_read(self):
-        self.transaction2[self.key2] = self.value2
+        self.transaction2[self.key3] = self.value3
         self.transaction2.commit()
-        self.assertIn(self.key2, self.transaction1)
-        self.assertEqual(self.transaction1[self.key2], self.value2)
+        self.assertIn(self.key3, self.transaction1)
+        self.assertEqual(self.transaction1[self.key3], self.value3)
 
 
 class RepeatableReadTransactionTestCase(TransactionTestsMixin, TestCase):
     isolation_level = IsolationLevel.REPEATABLE_READ
-
-    def test_cannot_occur_lost_update(self):
-        self.transaction1[self.key1] = self.value2
-        self.transaction2[self.key1] = self.value3
-        self.transaction2.commit()
-        with self.assertRaises(SerializationError):
-            self.transaction1.commit()
-
-    def test_cannot_occur_dirty_read(self):
-        self.transaction2[self.key1] = self.value2
-        self.assertEqual(self.transaction1[self.key1], self.value1)
 
     def test_cannot_occur_non_repeatable_read(self):
         self.transaction2[self.key1] = self.value2
@@ -122,17 +111,6 @@ class RepeatableReadTransactionTestCase(TransactionTestsMixin, TestCase):
 class SerializableTransactionTestCase(TransactionTestsMixin, TestCase):
     isolation_level = IsolationLevel.SERIALIZABLE
 
-    def test_cannot_occur_lost_update(self):
-        self.transaction1[self.key1] = self.value2
-        self.transaction2[self.key1] = self.value3
-        self.transaction2.commit()
-        with self.assertRaises(SerializationError):
-            self.transaction1.commit()
-
-    def test_cannot_occur_dirty_read(self):
-        self.transaction2[self.key1] = self.value2
-        self.assertEqual(self.transaction1[self.key1], self.value1)
-
     def test_cannot_occur_non_repeatable_read(self):
         self.transaction2[self.key1] = self.value2
         self.transaction2.commit()
@@ -143,7 +121,7 @@ class SerializableTransactionTestCase(TransactionTestsMixin, TestCase):
         self.transaction2.commit()
         self.assertNotIn(self.key3, self.transaction1)
 
-    def test_cannot_occur_serializable_error_set_key(self):
+    def test_cannot_occur_serializable_error_when_cross_write_keys(self):
         self.transaction1[self.key1] = self.value2
         _ = self.transaction1[self.key2]
         self.transaction2[self.key2] = self.value1
@@ -152,26 +130,20 @@ class SerializableTransactionTestCase(TransactionTestsMixin, TestCase):
         with self.assertRaises(SerializationError):
             self.transaction2.commit()
 
-    def test_can_commit_after_read_some_key(self):
+    def test_can_commit_when_concurrent_read_some_key(self):
         _ = self.transaction1[self.key2]
         _ = self.transaction2[self.key2]
         self.transaction1.commit()
         self.assertIsNone(self.transaction2.commit())
 
-    def test_can_commit_after_write_some_key_and_eq_value(self):
-        self.transaction1[self.key1] = self.value3
-        self.transaction2[self.key1] = self.value3
-        self.transaction1.commit()
-        self.assertIsNone(self.transaction2.commit())
-
-    def test_cannot_add_key_after_check_len(self):
+    def test_cannot_occur_serializable_error_when_write_key_after_check_len(self):
         _ = len(self.transaction1)
         self.transaction2[self.key3] = self.value3
         self.transaction2.commit()
         with self.assertRaises(SerializationError):
             self.transaction1.commit()
 
-    def test_serializable_error_1(self):
+    def test_cannot_occur_serializable_error_when_cross_read_and_writing(self):
         _ = self.transaction1[self.key1]
         _ = self.transaction1[self.key2]
         self.transaction2[self.key2] = self.value1
@@ -180,34 +152,21 @@ class SerializableTransactionTestCase(TransactionTestsMixin, TestCase):
         with self.assertRaises(SerializationError):
             self.transaction1.commit()
 
-    def test_serializable_error_2(self):
-        self.transaction1[self.key1] = self.value3
-        self.transaction2[self.key1] = self.value3
-        self.transaction1.commit()
-        self.assertIsNone(self.transaction2.commit())
-
-    def test_serializable_error_3(self):
+    def test_cannot_occur_serializable_error_when_writing_after_full_iter(self):
         _ = list(self.transaction1)
         self.transaction2[self.key3] = self.value3
         self.transaction2.commit()
         with self.assertRaises(SerializationError):
             self.transaction1.commit()
 
-    def test_serializable_error_4(self):
-        _ = len(self.transaction1)
-        self.transaction2[self.key3] = self.value3
-        self.transaction2.commit()
-        with self.assertRaises(SerializationError):
-            self.transaction1.commit()
-
-    def test_serializable_error_5(self):
+    def test_can_commit_when_read_another_than_part_iter_key(self):
         it = iter(self.transaction1)
         _ = next(it)
         self.transaction2[self.key3] = self.value3
         self.transaction2.commit()
         self.assertIsNone(self.transaction1.commit())
 
-    def test_serializable_error_6(self):
+    def test_cannot_occur_serializable_error_when_read_part_iter_key(self):
         it = iter(self.transaction1)
         read_key = next(it)
         self.transaction2[read_key] = self.value3
@@ -215,21 +174,21 @@ class SerializableTransactionTestCase(TransactionTestsMixin, TestCase):
         with self.assertRaises(SerializationError):
             self.transaction1.commit()
 
-    def test_serializable_error_7(self):
+    def test_can_commit_when_len_is_not_changed(self):
         _ = len(self.transaction1)
         self.transaction2[self.key3] = self.value3
         del self.transaction2[self.key2]
         self.transaction2.commit()
         self.assertIsNone(self.transaction1.commit())
 
-    def test_serializable_error_8(self):
+    def test_cannot_occur_serializable_error_when_write_key_after_negative_check_contains(self):
         _ = self.key3 in self.transaction1
         self.transaction2[self.key3] = self.value3
         self.transaction2.commit()
         with self.assertRaises(SerializationError):
             self.transaction1.commit()
 
-    def test_serializable_error_9(self):
+    def test_cannot_occur_serializable_error_when_write_key_after_positive_check_contains(self):
         _ = self.key2 in self.transaction1
         self.transaction2[self.key2] = self.value3
         self.transaction2.commit()
